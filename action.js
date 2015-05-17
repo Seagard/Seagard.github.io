f3a00f186288b966ebfb74a05c78f382c863daf8
Seagard <igorpostrygan@gmail.com>
2015-05-18 01:17:27 +0300

    commit

--- action.js
+++ action.js
@@ -1,58 +1,54 @@
 
 $(document).ready(function() {
 	var IDs = [];
-$("#faces").find("img").each(function(){ IDs.push(this.id); });
-   /*movement('TO');*/for (var i=0; i<IDs.length; i++) {
+	$("#faces").find("img").each(function(){ IDs.push(this.id); });
+   for (var i=0; i<IDs.length; i++) {
    		movement(IDs[i]);
    }
 });
 
 
-
-function movement(id) {
-	
+function movement(id) {	
 	$(document).mousemove(function(event) {	
+		var top = $('#' + id).offset().top;
+		var left = $('#' + id).offset().left;
+		var right = left + 188;
+		var bottom = top + 188;
 
-			var top = $('#' + id).offset().top;
-			var left = $('#' + id).offset().left;
-			var right = left + 188;
-			var bottom = top + 188;
-
-	//$( "#faces" ).children( ".photo" )
 
     if(event.pageX > right-62) { 
- 	$('#' + id).attr("src", "photos/" + id + "r.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "r.jpg");
  	}
  	
  	if(event.pageX < left+62) {
- 	$('#' + id).attr("src", "photos/" + id + "l.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "l.jpg");
 	}
 	if(event.pageX >= left+62 && event.pageX <= right-62 && event.pageY >= top+62 && event.pageY <= bottom-62) {
 		$('#' + id).attr("src", "photos/" + id + ".jpg");
 	}
 
 	if(event.pageY < top+62) {
- 	$('#' + id).attr("src", "photos/" + id + "up.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "up.jpg");
 	}
 
 	if(event.pageY > bottom-62) {
- 	$('#' + id).attr("src", "photos/" + id + "down.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "down.jpg");
 	}
 
 	if(event.pageY < top+62 && event.pageX > right-62) {
- 	$('#' + id).attr("src", "photos/" + id + "ur.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "ur.jpg");
 	}
 
 	if(event.pageY < top+62 && event.pageX < left+62) {
- 	$('#' + id).attr("src", "photos/" + id + "ul.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "ul.jpg");
 	}
 
 	if(event.pageY > bottom-62 && event.pageX < left+62) {
- 	$('#' + id).attr("src", "photos/" + id + "ll.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "ll.jpg");
 	}
 
 	if(event.pageY > bottom-62 && event.pageX > right-62) {
- 	$('#' + id).attr("src", "photos/" + id + "lr.jpg");
+ 		$('#' + id).attr("src", "photos/" + id + "lr.jpg");
 	}
 	});
 }
